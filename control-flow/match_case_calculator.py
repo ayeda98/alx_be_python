5497ae3037b8ae21f8f7d375cadb5a4e50cda762
Ayeda <ayedafrancsoi1998@gmail.com>
2024-09-23 09:21:34 +0100

    add macth_case.py

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,11 @@
+num1 = int(input("Enter the first number: "))
+num2 = input("Enter the second number: ")
+operation = input("Choose the operation (+, -, *, /): ")
+
+match operation:
+    case '+' : 
+        print("The result is ", num1 + num2," .")
+    case '-' : print("The result is ", num2 + num1," .")
+    case '*' : print("The result is ", num1 * num2," .")
+    case '/' : print("The result is ", num1 / num2," .")
+    case _: print("invalid choice")
