f5ae862158b30e1e9be472dae06d5b9bffd93dff
Ayeda98 <ayedafrancois1998@gmail.com>
2024-10-04 16:55:09 +0100

    add bank8account.py

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,16 @@
+class BankAccount:
+    def __init__(self, balance):
+        self.balance = balance
+    def deposit(self, amount):
+         self.balance =+ amount
+         return True
+    def withdraw(self, amount):
+        if (self.balance>=amount):
+             self.balance -= amount
+             return True
+        else : 
+             return False
+    def display_balance(self):
+         print(f"Current Balance: ${self.balance}")
+
+ 
