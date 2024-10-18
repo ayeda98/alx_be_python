db285193dc58ec5906cf0cebc5a152703cfa107e
Ayeda <ayedafrancsoi1998@gmail.com>
2024-10-18 17:31:38 +0100

    update

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,35 +1,47 @@
-class VoitureDeCourse(Voiture):  # Hérite de la classe Voiture
-    def __init__(self, couleur, marque, vitesse_max):
-        super().__init__(couleur, marque)  # Appelle le constructeur de la classe parente
-        self.vitesse_max = vitesse_max  # Attribut spécifique à VoitureDeCourse
-
-#principal class
+# Principal class
 class Book:
-    def __init__(self, title, author):
-        self.titel = title
+    def __init__(self, title, author, year=None):
+        self.title = title 
         self.author = author
+        self.year = year 
 
-#
+    def __repr__(self):
+        return f"Book('{self.title}', '{self.author}', {self.year})"
+    
+    def __str__(self):
+        return f"{self.title} by {self.author}, published in {self.year}"
+
+
+# Sous-classe pour les eBooks
 class EBook(Book):
-    def __init__(self, title, author, size):
-        super().__init__(title, author)
+    def __init__(self, title, author, size, year=None):
+        super().__init__(title, author, year)  
         self.size = size
 
+    def __str__(self):
+        return f"{self.title} by {self.author}, published in {self.year}, size: {self.size}MB"
+
+
+# Sous-classe pour les livres imprimés
 class PrintBook(Book):
-    def __init__(self, title, author, page_count):
-        super().__init__(title, author)
+    def __init__(self, title, author, page_count, year=None):
+        super().__init__(title, author, year)  
         self.page_count = page_count
 
+    def __str__(self):
+        return f"{self.title} by {self.author}, published in {self.year}, {self.page_count} pages"
+
+
+# Classe Bibliothèque
 class Library:
     def __init__(self):
         self.books = []
 
     def add_book(self, book):
-        if isinstance(book, Book)
+        if isinstance(book, Book):
             self.books.append(book)
 
-  
-    def List_book(self):
+    def list_books(self):
         for book in self.books:
-            print(book)
+            print(str(book))
 
