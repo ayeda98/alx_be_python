085379370abbed21c59f7b670ec8aac51572da8a
Ayeda <ayedafrancsoi1998@gmail.com>
2024-10-19 05:52:58 +0100

    update

--- Intro_to_DB/alx_book_store.sql
+++ Intro_to_DB/alx_book_store.sql
@@ -6,21 +6,21 @@ CREATE table book(book_id (Primary Key),
 title VARCHAR(130),
 author_id (Foreign Key referencing Authors table),
 price DOUBLE,
-publication_date DATE)
+publication_date DATE);
 
 CREATE TABLE Authors (author_id (Primary Key),
-author_name VARCHAR(215))
+author_name VARCHAR(215));
 
 CREATE TABLE Customers (customer_id (Primary Key),
 customer_name VARCHAR(215),
 email VARCHAR(215),
-address TEXT)
+address TEXT);
 
 CREATE TABLE Orders(order_id (Primary Key),
 customer_id (Foreign Key referencing Customers table),
-order_date DATE)
+order_date DATE);
 
 CREATE TABLE Order_Details (orderdetailid (Primary Key),
 order_id (Foreign Key referencing Orders table),
 book_id (Foreign Key referencing Books table),
-quantity DOUBLE)
+quantity DOUBLE);
