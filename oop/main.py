9985e9bd21a8ff2c5e9f0e2b8ebf6efb0d5f7aed
Ayeda <ayedafrancsoi1998@gmail.com>
2024-10-19 05:38:38 +0100

    3

--- oop/main.py
+++ oop/main.py
@@ -20,7 +20,7 @@ from library_system import Book, EBook, PrintBook, Library
     if __name__ == "__main__":
         main()"""
 
-from polymorphism_demo import Shape, Rectangle, Circle
+"""from polymorphism_demo import Shape, Rectangle, Circle
 import math
 
 def main():
@@ -31,6 +31,19 @@ def main():
 
     for shape in shapes:
         print(f"The area of the {shape.__class__.__name__} is: {shape.area()}")
+
+if __name__ == "__main__":
+    main()"""
+from class_static_methods_demo import Calculator
+
+def main():
+    # Using the static method
+    sum_result = Calculator.add(10, 5)
+    print(f"The sum is: {sum_result}")
+
+    # Using the class method
+    product_result = Calculator.multiply(10, 5)
+    print(f"The product is: {product_result}")
 
 if __name__ == "__main__":
     main()
