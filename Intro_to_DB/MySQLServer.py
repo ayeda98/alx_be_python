b55568d061f785e89ef3b3fb89ae6d1e786d57b9
Ayeda François DANIEL <ayeda@Ayedas-MacBook-Air.local>
2024-10-24 16:22:42 +0100

    ok

---
+++ Intro_to_DB/MySQLServer.py
@@ -0,0 +1,30 @@
+import mysql.connector
+from mysql.connector import Error
+
+def create_database():
+    try:
+        conn = mysql.connector.connect(
+            host='localhost',   
+            user='root',         
+            password=''  
+        )
+
+        if conn.is_connected():
+            cursor = conn.cursor()
+
+            cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+            
+            print("Database 'alx_book_store' created successfully!")
+        
+    except Error as e:
+        print(f"Error: '{e}'")
+    
+    finally:
+        if conn.is_connected():
+            cursor.close()
+            conn.close()
+            print("MySQL connection is closed.")
+
+if __name__ == '__main__':
+    create_database() 
+    
